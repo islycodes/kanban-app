cb4be204c1e524f29236ebba9b0064d86018e5ba
Felipe Gabriel Botelho <felipeg.bot@gmail.com>
2023-12-02 16:38:16 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { TicketStatusEnum } from "@/enums";
 
 class ApiServiceClass {
   Api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "http://192.168.3.105:5000",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
